e2deec5767f0597a8fd143dc725efac78e973140
pratul4875 <pratul.kumar@capgemini.com>
2024-12-16 01:14:52 +0530

    OrderModel.cs

---
+++ CyberShoppeeWebClient/Models/OrderModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CyberShoppeeWebClient.Models
+{
+    public class OrderModel
+    {
+       public int OrderId { get; set; }
+       public int CustomerId { get; set; }
+       public DateTime OrderDate { get; set; }
+       public DateTime ShipDate { get; set; }
+    }
+}
