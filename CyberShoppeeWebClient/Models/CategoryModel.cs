bae376504ac42656c60405979d2d96e085d57cb4
pratul4875 <pratul.kumar@capgemini.com>
2024-12-16 01:21:30 +0530

    CategoryModel.cs

---
+++ CyberShoppeeWebClient/Models/CategoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CyberShoppeeWebClient.Models
+{
+    public class CategoryModel
+    {
+       public int CategoryId { get; set; }
+       public string CategoryName { get; set; }
+       public string Description { get; set; }
+    }
+}
