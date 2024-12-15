6f2c2617ba02f8a7b210fa99a42fcaa66a21bdf1
pratul4875 <pratul.kumar@capgemini.com>
2024-12-16 01:11:54 +0530

    ProductModel.cs

--- CyberShoppeeWebClient/Models/ProductModel.cs
+++ CyberShoppeeWebClient/Models/ProductModel.cs
@@ -1,6 +1,17 @@
-public int ProductId { get; set; }
-public int CategoryId { get; set; }
-public string ModelNumber { get; set; }
-public string ModelName { get; set; }
-public decimal UnitCost { get; set; }
-public string Description { get; set; }
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CyberShoppeeWebClient.Models
+{
+    public class ProductModel
+    {
+      public int ProductId { get; set; }
+      public int CategoryId { get; set; }
+      public string ModelNumber { get; set; }
+      public string ModelName { get; set; }
+      public decimal UnitCost { get; set; }
+      public string Description { get; set; }
+    }
+}
