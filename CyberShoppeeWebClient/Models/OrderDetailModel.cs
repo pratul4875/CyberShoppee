c310da6474738ff7eb9a074e44c4d697112b6708
pratul4875 <pratul.kumar@capgemini.com>
2024-12-16 01:04:44 +0530

    OrderDetailModel.cs

---
+++ CyberShoppeeWebClient/Models/OrderDetailModel.cs
@@ -0,0 +1,5 @@
+public int OrderDetailId { get; set; }
+public int OrderId { get; set; }
+public int ProductId { get; set; }
+public int Quantity { get; set; }
+public decimal UnitCost { get; set; }
