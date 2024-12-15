ced8c2365c8dab69688cd7aedfcec2741745465e
pratul4875 <pratul.kumar@capgemini.com>
2024-12-16 01:10:24 +0530

    ProductModel.cs

---
+++ CyberShoppeeWebClient/Models/ProductModel.cs
@@ -0,0 +1,6 @@
+public int ProductId { get; set; }
+public int CategoryId { get; set; }
+public string ModelNumber { get; set; }
+public string ModelName { get; set; }
+public decimal UnitCost { get; set; }
+public string Description { get; set; }
