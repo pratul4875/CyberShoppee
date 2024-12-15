a16446a881dda5ad0b9b9570584a63911b4e14be
pratul4875 <pratul.kumar@capgemini.com>
2024-12-16 01:05:07 +0530

    RegistrationModel.cs

--- CyberShoppeeWebClient/Models/RegistrationModel.cs
+++ CyberShoppeeWebClient/Models/RegistrationModel.cs
@@ -7,6 +7,7 @@ namespace CyberShoppeeWebClient.Models
 {
     public class LoginModel
     {
+        public int CustomerId { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
