8886551296299c5b27aaa0a24a2ba3e818128f64
pratul4875 <pratul.kumar@capgemini.com>
2024-12-16 01:17:51 +0530

    ShoppingCartModel.cs

---
+++ CyberShoppeeWebClient/Models/ShoppingCartModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CyberShoppeeWebClient.Models
+{
+    public class ShoppingCartModel
+    {
+       public int RecordId { get; set; }
+       public int CartId { get; set; }
+       public int Quantity { get; set; }
+       public int ProductId { get; set; }
+       public DateTime DateCreated { get; set; }
+    }
+}
